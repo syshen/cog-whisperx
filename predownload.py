d703c42a4c14ad8899d35ce31bba31e7cae56185
Steven Shen <sysheen@gmail.com>
2023-10-02 08:43:42 +0800

    predownload model

---
+++ predownload.py
@@ -0,0 +1,8 @@
+from pyannote.audio import Pipeline
+from typing import Optional, Union
+import torch
+
+model_name="pyannote/speaker-diarization-3.0"
+device="gpu"
+use_auth_token="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"
+Pipeline.from_pretrained(model_name, use_auth_token=use_auth_token).to(device)
