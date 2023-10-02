4a05cc4347411457516ba2a20ab3c93d20688a60
Steven Shen <sysheen@gmail.com>
2023-10-02 08:53:26 +0800

    fix

--- predownload.py
+++ predownload.py
@@ -1,7 +1,8 @@
 from pyannote.audio import Pipeline
 from typing import Optional, Union
 import torch
+import sys
 
 model_name="pyannote/speaker-diarization-3.0"
-use_auth_token="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"
+use_auth_token=sys.argv[1]
 Pipeline.from_pretrained(model_name, use_auth_token=use_auth_token)
