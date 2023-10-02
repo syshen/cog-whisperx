dab9e4c566b9936d63f3da2b8b586622bc1abc51
Steven Shen <sysheen@gmail.com>
2023-10-02 20:57:48 +0800

    add pyannote/segmentation

--- predownload.py
+++ predownload.py
@@ -6,3 +6,4 @@ import sys
 model_name="pyannote/speaker-diarization-3.0"
 use_auth_token=sys.argv[1]
 Pipeline.from_pretrained(model_name, use_auth_token=use_auth_token)
+Pipeline.from_pretrained("pyannote/segmentation", use_auth_token=use_auth_token)
