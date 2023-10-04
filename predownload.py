69cd95dfbb6dfa331a4501de889e66c9143a1067
Steven Shen <sysheen@gmail.com>
2023-10-04 08:55:16 +0800

    try predownload segment

--- predownload.py
+++ predownload.py
@@ -1,9 +1,8 @@
-from pyannote.audio import Pipeline
-from typing import Optional, Union
+from pyannote.audio import Pipeline, Model
 import torch
 import sys
 
 model_name="pyannote/speaker-diarization-3.0"
 use_auth_token=sys.argv[1]
 Pipeline.from_pretrained(model_name, use_auth_token=use_auth_token)
-Pipeline.from_pretrained("pyannote/segmentation", use_auth_token=use_auth_token)
+Model.from_pretrained("pyannote/segmentation", use_auth_token=use_auth_token,  strict=False)
