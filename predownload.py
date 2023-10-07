22a73b5d96f68c1fc1dc7244170f6f73b639887d
Steven Shen <sysheen@gmail.com>
2023-10-07 17:41:53 +0800

    remove unnecessary predownload script

--- predownload.py
+++
@@ -1,8 +0,0 @@
-from pyannote.audio import Pipeline, Model
-import torch
-import sys
-
-model_name="pyannote/speaker-diarization-3.0"
-use_auth_token=sys.argv[1]
-Pipeline.from_pretrained(model_name, use_auth_token=use_auth_token)
-Model.from_pretrained("pyannote/segmentation", use_auth_token=use_auth_token,  strict=False)
